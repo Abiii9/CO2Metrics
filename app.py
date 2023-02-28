5d3de250f49a65364261c7142d125a646ddd5c87
Abiii9 <abinayae19@gmail.com>
2023-02-28 12:26:34 +0000

    final commit before pushing to render

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def metrics():
     conn.close()
     year = None
     noofcolumns = 0
+    noofrows = 0
     industry = None
     if(request.args):
         country = request.args['country']
@@ -35,19 +36,24 @@ def metrics():
         if(not year and not industry):
             data = cur.execute("SELECT emissions.Indicator,industries.IndustryName,emissions.F2008,emissions.F2009,emissions.F2010,emissions.F2011,emissions.F2012,emissions.F2013,emissions.F2014,emissions.F2015,emissions.F2016,emissions.F2017,emissions.F2018 FROM emissions LEFT OUTER JOIN industries ON emissions.Industry_id = industries.Industry_id WHERE Country_id = ?", country_id).fetchall()
             noofcolumns = 13
+            noofrows = 45
         elif(year and not industry):
             data = cur.execute(f"SELECT emissions.Indicator,industries.IndustryName,emissions.{year} FROM emissions LEFT OUTER JOIN industries ON emissions.Industry_id = industries.Industry_id WHERE Country_id = ?", country_id).fetchall()
             noofcolumns = 3
+            noofrows = 45
         elif(not year and industry):
-            industry_id = cur.execute("SELECT Industry_id FROM industries WHERE IndustryName = ?", [industry])
-            data = cur.execute(f"SELECT emissions.Indicator,industries.IndustryName,emissions.F2008,emissions.F2009,emissions.F2010,emissions.F2011,emissions.F2012,emissions.F2013,emissions.F2014,emissions.F2015,emissions.F2016,emissions.F2017,emissions.F2018 FROM emissions LEFT OUTER JOIN industries ON emissions.Industry_id = industries.{industry_id} WHERE Country_id = ?", country_id).fetchall()
+            industry_id = cur.execute("SELECT Industry_id FROM industries WHERE IndustryName = ?", [industry]).fetchone()
+            data = cur.execute(f"SELECT emissions.Indicator,industries.IndustryName,emissions.F2008,emissions.F2009,emissions.F2010,emissions.F2011,emissions.F2012,emissions.F2013,emissions.F2014,emissions.F2015,emissions.F2016,emissions.F2017,emissions.F2018 FROM emissions LEFT OUTER JOIN industries ON emissions.Industry_id = industries.Industry_id WHERE Country_id = ? AND emissions.Industry_id = ?",(country_id[0],industry_id[0])).fetchone()
+            print(data)
             noofcolumns = 13
+            noofrows = 1
         else:
-            industry_id = cur.execute("SELECT Industry_id FROM industries WHERE IndustryName = ?", [industry])
-            data = cur.execute(f"SELECT emissions.Indicator,industries.IndustryName,emissions.{year} FROM emissions LEFT OUTER JOIN industries ON emissions.Industry_id = industries.{industry_id} WHERE Country_id = ?", country_id).fetchall()
+            industry_id = cur.execute("SELECT Industry_id FROM industries WHERE IndustryName = ?", [industry]).fetchone()
+            data = cur.execute(f"SELECT emissions.Indicator,industries.IndustryName,emissions.{year} FROM emissions LEFT OUTER JOIN industries ON emissions.Industry_id = industries.Industry_id WHERE Country_id = ? AND emissions.Industry_id = ?", (country_id[0],industry_id[0])).fetchone()
             noofcolumns = 3
+            noofrows = 1
         conn.close()
     else:
         data=None
-    return render_template('metrics.html', rows=rows,names=name[5:],data=data,industries=industries,year=year,columns=noofcolumns)
+    return render_template('metrics.html', rows=rows,names=name[5:],data=data,industries=industries,year=year,columns=noofcolumns, industry=industry)
 
